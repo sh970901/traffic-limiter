5e1410602cc12934cfa7a7a29f0de7eedd72e30b
lee_seunghun06 <lee_seunghun06@elandinnople.com>
2024-12-11 19:01:56 +0900

    etc: first commit

---
+++ src/main/java/com/blog4j/limiter/TrafficLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.blog4j.limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficLimiterApplication.class, args);
+	}
+
+}
